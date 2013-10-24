5fe75f16df81ccf60bb9a247f09de90f3f2c6cdb
Mike Leone <mleone896@gmail.com>
2013-10-23 21:30:30 -0400

    ruby script playing around with rye

---
+++ remote_exec.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'optparse'
+require 'pp'
+require 'ostruct'
+require 'rye'
+require 'highline/import'
+
+class ParseArgs
+    def self.parse(args)
+        options = OpenStruct.new
+        options.server = []
+        opt_parser = OptionParser.new do |opt|
+          opt.banner = "Usage: #{File.basename($0)} OPTIONS"
+          opt.separator ""
+          opt.separator "OPTIONS"
+          opt.on("-S", "--server SERVER ", Array,  "which facter fact you would like to use") do |s|
+            options.server << s
+          end
+          opt.on("-h", "--help", "help") do
+            puts opt_parser
+            exit
+          end
+          opt.on("-T", "--path DIRECTORY", "Path on remote server")  do |t|
+              options.path = t
+          end
+          opt.on("-F", "--file FILE", "filename to be moved") do |f|
+              options.file = f
+          end
+          opt.on("--flow upload/download", "Which way the file is going") do |flow|
+              options.flow = flow
+          end
+          opt.on("-b", "--batch BATCHMODE", "Create a list of servers from a file") do |batch|
+              options.batch = batch
+           end
+          opt.on("-P", "--pasword", "Your ssh password") do
+              options.password = ask("Enter your password:  ") { |q| q.echo = "*" }
+          end
+        end
+       opt_parser.parse!(args)
+       options
+    end # end parse()
+end # close class
+
+
+class FileOperations
+  attr_accessor :file, :path
+  def initalize(file, path)
+      @file = file
+      @path = path
+  end
+  def file_upload(file, path)
+     $rset.file_upload file, path
+  end
+
+  def file_download(file, path)
+      puts $rset.hostname
+      $rset.file_download "#{path}/#{file}", file
+      ret = File.exists?("#{file}")
+           if !ret
+               puts "#{file} did not transfer correctly"
+               exit 2
+           end
+  end
+
+end
+
+options = ParseArgs.parse(ARGV)
+fileoperators = FileOperations.new
+password = options.password
+
+# grab the hosts in question from command line args
+  if options.batch.to_s == "true"
+     hostfile = ask("where is the file containing hosts ?:  ") { |q| q.echo = true }
+     hosts = IO.readlines(hostfile)
+     hosts.map! {|x| x.chomp}
+  else
+    hosts = options.server
+  end
+# these need to be global for the scope of class methods
+options.each do |j|
+    pp j
+end
+$rset = Rye::Set.new "setname", :password_prompt => false, :user => "mleone", \
+    :parallel => false, :password => "#{password}"
+$rset.add_boxes hosts
+
+ if !options.path.nil? and !options.file.nil? and options.flow == "upload"
+   fileoperators.file_upload(options.file, options.path)
+ end
+
+ if !options.path.nil? and !options.file.nil? and options.flow == "download"
+     fileoperators.file_download(options.file, options.path)
+ end
+
+
+
